1efdc6ec3b7179658b5c59a30f382b5d42b44161
Vladimir Zorov <vl.zorov@gmail.com>
2023-02-15 21:51:56 +0200

    Add changes

--- src/main/java/aquarium/entities/aquariums/BaseAquarium.java
+++ src/main/java/aquarium/entities/aquariums/BaseAquarium.java
@@ -5,63 +5,83 @@ import aquarium.entities.fish.Fish;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
-public abstract class BaseAquarium implements Aquarium{
+import static aquarium.common.ConstantMessages.NOT_ENOUGH_CAPACITY;
+import static aquarium.common.ExceptionMessages.*;
+
+public abstract class BaseAquarium implements Aquarium {
 
     private String name;
     private int capacity;
     private Collection<Decoration> decorations;
-    private Collection<Fish> fish;
+    private Collection<Fish> fishes;
 
     public BaseAquarium(String name, int capacity) {
-        this.name = name;
+        this.setName(name);
         this.capacity = capacity;
         this.decorations = new ArrayList<>();
-        this.fish = new ArrayList<>();
+        this.fishes = new ArrayList<>();
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(AQUARIUM_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
     }
 
     @Override
     public int calculateComfort() {
-        return 0;
+        return this.decorations.stream().mapToInt(Decoration::getComfort).sum();
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public void addFish(Fish fish) {
-
+        if (fishes.size() < capacity && fishes.contains(fish) ) {
+            fishes.add(fish);
+        } else {
+            throw new IllegalArgumentException(NOT_ENOUGH_CAPACITY);
+        }
     }
 
     @Override
     public void removeFish(Fish fish) {
-
+        fishes.remove(fish);
     }
 
     @Override
     public void addDecoration(Decoration decoration) {
-
+        decorations.add(decoration);
     }
 
     @Override
     public void feed() {
-
+        fishes.stream().forEach(Fish::eat);
     }
 
     @Override
     public String getInfo() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s %s:", name, name)).append(System.lineSeparator());
+        sb.append(String.format("Fish: ")).append(System.lineSeparator());
+        sb.append(String.format("Decorations: %d", decorations.size())).append(System.lineSeparator());
+        sb.append(String.format("Comfort: %d", calculateComfort())).append(System.lineSeparator());
+        return sb.toString().trim();
     }
 
     @Override
     public Collection<Fish> getFish() {
-        return null;
+        return Collections.unmodifiableCollection(fishes);
     }
 
     @Override
     public Collection<Decoration> getDecorations() {
-        return null;
+        return Collections.unmodifiableCollection(decorations);
     }
 }
